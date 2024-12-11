5abbad05dfb41e579ec4b10a4cf408fdab7261aa
alvaroocaa <145217472+alvaroocaa@users.noreply.github.com>
2024-12-11 22:31:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='DatSci_libary',
-    version='0.1.0',
-    description='A short description of my library',
+    name='datsci_library',
+    version='0.1',
+    description='A set of functions to help me on rutinary work tasks as data specialist, this is a beta phase as it is my first ever creation of a python library. Please don't judge :)',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='Alvaro Castillejo',
     url='https://github.com/alvaroocaa/DatSci_library',
-    packages=find_packages(),  # This will automatically discover all packages
+    packages=find_packages(),  
     install_requires=[
-        # List any dependencies here, e.g.:
-        # 'numpy',
+        'numpy',
+        'pandas',
+        'sys',
+        'os'
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
