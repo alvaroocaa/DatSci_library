6d00e2eecd99c07798bb4b82b363c2e44761662f
alvaroocaa <alvaro.castillejoa@gmail.com>
2025-01-31 17:22:01 +0100

    aa

--- datsci/datsci.py
+++ datsci/datsci.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd # type: ignore
 import numpy as np # type: ignore
-import openpyxl
+import openpyxl # type: ignore
 import tempfile
 
 def extract_df(file, directory, filename, ext):
